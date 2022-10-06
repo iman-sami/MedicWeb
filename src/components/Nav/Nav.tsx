ed0fdc5cf36afa449a6a69dd0918b22357b210a5
iman-sami <imansamifaith@gmail.com>
2022-10-06 22:07:15 +0300

    nav updated

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import styled from "styled-components";
+import {data} from "../Data/Navdata"
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <Container>
+        <Contents>
+            <h1 className='text-blue-800 font-bold'>Me<span className='text-blue-400'>dic</span></h1>
+            <div className='flex justfiy-evenly gap-3 '>
+                    <ul>
+                      {data && data.map((data,index)=>(
+                        <li key={index}>
+                            <a href={data.path}>{data.name}</a>
+                        </li>
+                      ))}
+                    </ul>
+            </div>
+        </Contents>
+    </Container>
   )
 }
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+`
+const Contents = styled.div`
+    margin: 0 auto;
+    max-width: 960px;
+    height: 70px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
+`
 export default Nav
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import Nav from '../components/Nav/Nav'
+const Landing = () => {
+  return (
+    <Container>
+        <Nav/>
+    </Container>
+  )
+}
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+
+
+`
+export default Landing
