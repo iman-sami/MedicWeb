53dd8cfb7af2d1701cd9b160923b1ed957f31cd4
iman-sami <imansamifaith@gmail.com>
2022-10-06 21:52:05 +0300

    updated data

--- src/components/Data/Navdata.ts
+++ src/components/Data/Navdata.ts
@@ -1,5 +1,19 @@
 export const data = [
-  {"name": "Home",
-   "path": "/"},
+  {
+       name: "Home",
+       path: "/"
+    },
+   {
+       name:"Service",
+       path: "/services"
+    },
+    {
+        name:"AboutUs",
+        path: "/about"
+    },
+    {
+        name:"Comunity",
+        path:"/comunity"
+    }
     
 ]
