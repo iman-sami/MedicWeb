3dd55cf240325a2761d0ea3a69a3b64f4aebaf0d
iman-sami <imansamifaith@gmail.com>
2022-10-20 22:18:06 +0300

    added svg files

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import Hero from '../components/Hero/Hero'
 import Nav from '../components/Nav/Nav'
+import Service from '../components/Service/Service'
 const Landing = () => {
   return (
     <Container>
         <Nav/>
+        <Hero/>
+        <Service/>
     </Container>
   )
 }
 const Container = styled.div`
     width: 100%;
-    height: 100%;
+    height: auto;
 
 
 `
