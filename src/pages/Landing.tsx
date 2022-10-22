b1197af7ad2ac5bfc59ebd607c0f1c655bd2d52d
iman-sami <imansamifaith@gmail.com>
2022-10-22 16:47:54 +0300

    landing page updated

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
 import Aboutus from '../components/AboutUs/Aboutus'
+import Footer from '../components/Footer/Footer'
 import Hero from '../components/Hero/Hero'
 import Nav from '../components/Nav/Nav'
 import OurTeam from '../components/OurTeam/OurTeam'
 import Service from '../components/Service/Service'
+import Subscribe from '../components/Subscribe/Subscribe'
 import Teams from '../components/Teams/Teams'
 import Testimonial from '../components/Testimonials/Testimonial'
 const Landing = () => {
@@ -16,11 +18,14 @@ const Landing = () => {
         <Aboutus/>
         <OurTeam/>
         <Testimonial/>
+        <Subscribe/>
+        <Footer/>
     </Container>
   )
 }
 const Container = styled.div`
     width: 100%;
+    height: auto;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
